720cdccd4f00543c4420225fd2db562846bef63e
Filipe Moraes Rodrigues <filipe.rodrigues@gestao.gov.br>
2026-03-24 16:32:25 -0300

    feat(backend): implementa endpoints de listagem e criacao em users-service

--- backend/users-service/main.py
+++ backend/users-service/main.py
@@ -1,7 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import get_db, Base, engine
+import models, schemas
+from passlib.context import CryptContext
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Users Service", version="1.0.0")
 
-@app.get("/")
-def read_root():
-    return {"message": "Users Service is running!"}
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+@app.post("/users/", response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter((models.User.email == user.email) | (models.User.username == user.username)).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email or Username already registered")
+    
+    hashed_password = get_password_hash(user.password)
+    new_user = models.User(username=user.username, email=user.email, hashed_password=hashed_password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@app.get("/users/", response_model=list[schemas.UserResponse])
+def list_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = db.query(models.User).offset(skip).limit(limit).all()
+    return users
+
